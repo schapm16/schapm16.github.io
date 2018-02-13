9167cd847496b36d6d3e6a9342ed70eb0bcce930
Stephen Chapman <Schapm16@gmail.com>
2018-02-13 12:19:34 -0500

    Hide sideNave on smaller screens.

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -12,19 +12,21 @@ $(function() {
 
 		// Reveal and Hide Side Nav
 		afterLoad: function (anchorlink){
-			if (anchorlink === 'front-page') {
-				$('#sideNav').animate({
-					opacity: '0',
-					right: '-200px'
-				}, 'slow', function() {
-					$('#sideNav').css('display','none');
-				});
-			} else {
-				$('#sideNav').css('display', 'block');
-				$('#sideNav').animate({
-					opacity: '1',
-					right: '0px'
-				}, 'slow');
+			if (screen.width > 1021) {
+				if (anchorlink === 'front-page') {
+					$('#sideNav').animate({
+						opacity: '0',
+						right: '-200px'
+					}, 'slow', function() {
+						$('#sideNav').css('display','none');
+					});
+				} else {
+					$('#sideNav').css('display', 'block');
+					$('#sideNav').animate({
+						opacity: '1',
+						right: '0px'
+					}, 'slow');
+				}
 			}
 		}
 	});

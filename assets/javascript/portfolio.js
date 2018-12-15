a1a9e19f1ebf99182226ebe1eac8d89dce6e7119
Stephen Chapman <Schapm16@gmail.com>
2018-12-15 11:51:59 -0500

    Convert tabs to spaces in code

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,37 +1,37 @@
 (function() {
-	const sideNav = document.getElementById('sideNav');
-	const storyContentRows = document.querySelectorAll('#storyContent .row');
+  const sideNav = document.getElementById('sideNav');
+  const storyContentRows = document.querySelectorAll('#storyContent .row');
 
-	// Configure fullPage.js
-	new fullpage('#fullpage', {
-		licenseKey: null,
-		anchors: ['front-page', 'story-page', 'work-page', 'contact-page'],
-		controlArrows: false,
-		fixedElements: '#sideNav',
-		menu: '#sideNav>ul',
-		scrollOverflow: true,
+  // Configure fullPage.js
+  new fullpage('#fullpage', {
+    licenseKey: null,
+    anchors: ['front-page', 'story-page', 'work-page', 'contact-page'],
+    controlArrows: false,
+    fixedElements: '#sideNav',
+    menu: '#sideNav>ul',
+    scrollOverflow: true,
 
-		afterLoad: function (origin, destination) {
-			// Reveal and Hide Side Nav
-			if (screen.width > 1021 && innerWidth > 1021) {
-				if (destination.anchor === 'front-page') {
-					sideNav.style.opacity = '';
-					sideNav.style.right = '';
-				} else {
-					sideNav.style.opacity = 1;
-					sideNav.style.right = '25px'
-				}
-			}
+    afterLoad: function (origin, destination) {
+      // Reveal and Hide Side Nav
+      if (screen.width > 1021 && innerWidth > 1021) {
+        if (destination.anchor === 'front-page') {
+          sideNav.style.opacity = '';
+          sideNav.style.right = '';
+        } else {
+          sideNav.style.opacity = 1;
+          sideNav.style.right = '25px'
+        }
+      }
 
-			// Animation for Story Page
-			if (destination.anchor === 'story-page') {
-				if (storyContentRows[0].style.opacity !== 1) {
-					storyContentRows.forEach((each, i) => {
-						each.style.transition = `opacity 0.5s linear ${0.2 + i * 0.2}s`;
-						each.style.opacity = 1;
-					})
-				}
-			}
-		}
-	});
+      // Animation for Story Page
+      if (destination.anchor === 'story-page') {
+        if (storyContentRows[0].style.opacity !== 1) {
+          storyContentRows.forEach((each, i) => {
+            each.style.transition = `opacity 0.5s linear ${0.2 + i * 0.2}s`;
+            each.style.opacity = 1;
+          })
+        }
+      }
+    }
+  });
 })();
